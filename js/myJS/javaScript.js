3a4c7a98a28d85d5784cbc1685b907591a61736f
alemukam <kamir.alemumi@inbox.lv>
2017-07-14 21:34:07 +0300

    first update

---
+++ js/myJS/javaScript.js
@@ -0,0 +1,77 @@
+$("#for-append").hide();
+
+var persons = [
+{img: "img/kimmo_alkio.jpg",
+name: "Kimmo Alkio",
+position: "President and CEO",
+html: "<h3>The City of Helsinki is piloting solutions for more real-time traffic planning and control and open data provision</h3><p>Helsinki’s City Planning Department is piloting a video analysis solution using mobile cameras for real-time forecasting of traffic and people flows as well as for offering services to city residents. A system was created for the pilot project for collecting, analysing and visualizing video material.<br>The City of Helsinki wants to improve its capability of obtaining a real-time view of traffic from multiple data sources. A real-time view of traffic is a graphic view of data related to traffic, lanes, the locations of means of transport, and conditions. It can provide a short-term forecast of traffic volumes, the smoothness of traffic, incidents, punctuality and prevailing conditions.<br>Situation data based on a real-time view plays a central role in improving the smoothness and predictability of daily journeys and transportation as well as traffic safety. Data can be transmitted to end users, service providers and others who can benefit from it. Traffic data collected over a longer period can be analysed so as to draw up forecasts of traffic flows and the effects of various incidents compared with the normal situation. Examples of incidents are traffic jams caused by major public events and accidents as well as poor weather conditions. Forecasts help in guiding traffic effectively immediately after an incident and preparing for the consequences. Real-time analysis of traffic data also enables better services for city residents. Data on vacant parking spaces can be distributed to motorists involved in the system. Video material also reveals when it is time to clear snow from bicycle and pedestrian paths. The Finnish Transport Agency collects travel time data, requiring a lag of no more than one minute from the actual situation.<br>It is expected that the investment costs needed to improve the quality of the real-time view can be recovered through reduced costs of accidents, congestion and emissions, as well as through new business operations. The real-time view makes it possible to address incidents due to their rapid identification, positioning and forecasting.</p>"
+},
+
+{img: "img/hakan_dahlstrom.jpg",
+name: "Håkan Dahlström",
+position: "Executive Vice President, TSM",
+html: "<h3>Tieto to develop new digitally empowered customer experience for FOREX Bank</h3><p>FOREX Bank has chosen Tieto for developing and renewing the customer experience in their digital channels. The solution includes a new interface for the internet bank and a mobile bank with full retail online banking service. The new delivery will give FOREX Bank a modern it-environment and a complete end-to-end banking solution delivered as a service. The new contract complements Tieto’s current core banking installation which has been used by FOREX Bank for seven years.<br>The first stage will entail a new retail online banking service including a new web and a mobile solution. By spring 2015, FOREX Bank should have a completely new digital banking experience in place.<br>Competition in the Nordic banking sector in optimizing the customer experience will accelerate in the coming years. If customers don’t get rewarding experiences every time they interact with an organisation, they will look for other alternatives. We are pleased to be chosen as partner for FOREX when they renew their digital channel experience. This is also a selected growth area for Tieto, says Per Johanson, Head of Financial Services, Tieto.<br>With our new solutions we will further strengthen the banking part of FOREX Bank’s proposition in Sweden, as well as supporting our market-leading position in travel money in the Nordic region. Tieto is consistently delivering good quality in our core banking solution. This made them our first choice when we wanted to refresh and renew the customer experience in our retail banking services, says Thomas Högväg, acting CEO at FOREX Bank.</p>"
+},
+
+{img: "img/lasse_heinonen.jpg",
+name: "Lasse Heinonen",
+position: "Chief Financial Officer",
+html: "<h3>Custom IT solutions for leading global technical company</h3><p>The leading technical company ÅF AB was looking for an IT partner to supply the company’s international offices. The IT solution had to be adaptable to their intentions to grow 15 percent every year and expand from 5 000 to 10 000 employees within the next three years. ÅF chose Tieto to facilitate the company’s expansion by delivering a global IT solution and a standardized and scalable infrastructure for the company’s 100 (and growing) offices. Tieto provided a new flexible cost model “Price-Per-Employee”. Since the new IT solution was introduced it has been easier for the company to calculate and predict costs when acquiring new companies.<br>Grow and expand within the next couple of years to be able to focus more on its core business and expand, ÅF hired Tieto as their IT partner back in 2007. ÅF managed to meet its goals, which contributed to new challenges in 2012. This time, the IT solution had to be more flexible to the company’s intentions to grow by 15 percent each year, and expand from 5 000 to 10 000 employees within the next three years. ÅF needed a common IT solution with high availability to the company’s global offices and at the same time be adaptable to the expansion of their business. The solution also needed to be adapted to improve efficiency for the employees with a unified global solution.<br>New infrastructure and flexible cost control model, “Price-Per-Employee”. A number of actions were taken to meet ÅF’s plan to expand its business. Tieto facilitated the company by providing global IT solutions such as infrastructure services and governance for its offices worldwide. Tieto implemented the standardized IT solution to provide a uniform structure for the companies. The new global IT platform was based on standardized service and also scalable in order to be adaptable to different countries’ needs. A new flexible cost model ”Price-Per-Employee” was also developed by Tieto to meet ÅF’s needs and make it easier for the company to predict and control IT costs in the acquisition of new companies.<br>The project included 100 offices in 20 countries in Europe, Asia, South America and Africa. The IT intensive and expanding company had great demands on their technical platform and Tieto as the provider. Therefore, it was essential for Tieto to back up ÅF with a complete IT solution, infrastructure and governance for its offices worldwide. Tieto implemented the standardized service that was scalable in order to be adaptable to different countries’ specific amendments, applications and services. Tieto’s development of “Price-Per-Employee” made it easier for ÅF to predict and control IT costs when acquiring new companies. The platform is based on a fixed price, “Employee All” which means that the fee is based on the company’s number of employees. More specific services can also be added to the service, country-by-country. For example: ÅF in Sweden got the most comprehensive service catalogue and add-on services per user in Sweden</p>"
+},
+
+{img: "img/per_johanson.jpg",
+name: "Per Johanson",
+position: "Executive Vice President, Financial Services",
+html: "<h3>New digital services promoting wellbeing at work</h3><p>Every company is currently thinking about digitalization – what it is and how best to implement it in their field of business. The fast-paced modern world demands new kinds of innovation from companies. There is a constant need to develop digital services that take into account the special needs of customers. The pension insurance company Elo began to develop digital service models and approaches for well-being at work with the Tieto Experience Hub innovation programme (XHub). XHub is based on fast innovation, the testing of ideas and validation by end-users. “XHub is an interesting programme. It combines two things: solving practical issues and finding a new way of development. At the same time, it allows us to see how we can be agile as an organisation,” Tikkanen describes.<br>“XHub supports innovation through networks and open interaction with Elo customers. This makes the development more efficient and ensures that the new services meet the needs of the users. Furthermore, we encourage Elo to step outside their comfort zone and recognise new business models from outside their sector as well,” says Business Consultant Fanny Vakkila from Tieto. The XHub programme stages can include, for example, innovation workshops with new partners, design projects, service design and the discovery of prototypes through hackathons. “Everything is based on trials and receiving immediate feedback. If something doesn’t work, we don’t hesitate to change direction. This lets us now early on whether the idea is good and whether there is an actual need for the service,” Tikkanen describes. Tieto experts have directed the development and the new working method. Furthermore, Tieto has compiled a network of different operators to innovate together with Elo. “Tieto has managed to find good people with vision both from technology and digital services. Our operation has become faster, as the partners understand our field,” Tikkanen states.<br>The development of digital solutions was initially concentrated on well-being at work, and then, as the XHub programme progressed, the focus shifted to Elo’s business customers. In terms of the number of insurance policies, Elo is the largest pension insurer of entrepreneurs in Finland, so this new solution will serve a great number of customers. Digital services help entrepreneurs as they can be used in the evenings or at weekends, when Elo cannot offer personal customer service. “We have received positive feedback on our personal service, so it is important to figure out how to get our digital services to the same level,” Tikkanen points out. According to Tikkanen, the participation of end-users, i.e. entrepreneurs, in every stage of the development has been important. Furthermore, XHub has combined different sectors of business within Elo. “Within the company, XHub has been a valuable project. It passes through the entire organisation and combines well-being experts, IT and business development. Everyone has been excited about the new approach and learned a lot,” Tikkanen explains. “It has been a treat to follow Elo’s journey in the XHub programme. Their courage and enthusiasm to try out new working methods has been an inspiration to the entire XHub team,” Vakkila says happily.<br>Owned by its customers, Elo is a pension insurance company that takes care of the statutory pension insurances of its customer company employees and entrepreneurs. Elo is the largest pension insurance company in Finland: a third of all Finnish companies and over 40% of entrepreneurs have chosen Elo to manage their pension insurances. Elo is responsible for the future pensions of around 500,000 employees and entrepreneurs, and takes care of 210,000 pensioners and around €20 billion worth of investments.</p>"
+},
+
+{img: "img/katariina_kravi.jpg",
+name: "Katarina Kravi",
+position: "Executive Vice President, Human Resources",
+html: "<h3>Digitalizing Norway's second biggest municipality</h3><p>The municipality of Bergen has signed an agreement with Tieto Norway to introduce a new case and records management solution for the entire municipality.<br>The solution forms the platform to offer extended and improved digital services to citizens, businesses and employees. The agreement has a term of 12 years. The agreement means that the municipality’s existing case and records management solution will be retired after 24 years of service. Although the old system has been further developed during these years, it had limitations that were not in line with current needs and aspirations in Bergen municipality.<br>The new solution will improve the quality and efficiency of case management and archiving procedures in Bergen. The solution supports the “digital first choice” methodology based on the principle that users should be able to interact electronically with the municipality around the clock. It provides increased self-service and automation of application processes. It further supports the municipality’s goal to make all relevant inquiries, applications, forms and reports open to digital completion and submission.<br>The solution also enables the municipality to receive and process various types of documents regardless of platform. The system will provide efficient document capture and retrieval. The municipality gets automated workflows, making way for standardized processes. The solution will be the baseline for all document production related to political and administrative activities within the municipality. In addition to being the core system for administrative proceedings, archives, governing documents, political meetings and board handling processes.<br>In accordance with local and national goals During the process, relevant stakeholders have taken part in the work. This is in line with the government's newly published white paper Digital Agenda. The report points out that the municipalities need to be heavily involved in the planning to make digitalization of public and citizen services a success. Bergen’s ICT strategy is based on the assumption that smart, efficient and targeted use of ICT will result in better service quality and easier dialogue with citizens, businesses, and organizations, and also ensure effective use of resources in the municipality.</p>"}
+];
+
+$("#alkiokim").on("click", function() {
+	$("#for-append").hide();
+	$("#for-append").fadeIn();
+	$("#img").attr("src", persons[0].img);
+	$("h4").text( persons[0].name);
+	$("#superP").text( persons[0].position);
+	$("#to-append").html(persons[0].html).appendTo("#for-append");
+});
+
+$("#dahlshak").on("click", function() {
+	$("#for-append").hide();
+	$("#for-append").fadeIn();
+	$("#img").attr("src", persons[1].img);
+	$("h4").text( persons[1].name);
+	$("#superP").text( persons[1].position);
+	$("#to-append").html(persons[1].html).appendTo("#for-append");
+});
+
+$("#heinolas").on("click", function() {
+	$("#for-append").hide();
+	$("#for-append").fadeIn();
+	$("#img").attr("src", persons[2].img);
+	$("h4").text( persons[2].name);
+	$("#superP").text( persons[2].position);
+	$("#to-append").html(persons[2].html).appendTo("#for-append");
+});
+
+$("#johanper").on("click", function() {
+	$("#for-append").hide();
+	$("#for-append").fadeIn();
+	$("#img").attr("src", persons[3].img);
+	$("h4").text( persons[3].name);
+	$("#superP").text( persons[3].position);
+	$("#to-append").html(persons[3].html).appendTo("#for-append");
+});
+
+$("#kravikat").on("click", function() {
+	$("#for-append").hide();
+	$("#for-append").fadeIn();
+	$("#img").attr("src", persons[4].img);
+	$("h4").text( persons[4].name);
+	$("#superP").text( persons[4].position);
+	$("#to-append").html(persons[4].html).appendTo("#for-append");
+});
